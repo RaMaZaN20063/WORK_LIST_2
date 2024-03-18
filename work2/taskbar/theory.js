43b3cf925060b7256bfddaa051086914c996ac5c
RaMaZaN20063 <162133144+RaMaZaN20063@users.noreply.github.com>
2024-03-18 14:16:25 +0600

    Add files via upload

---
+++ work2/taskbar/theory.js
@@ -0,0 +1,22 @@
+const names = ["Roma", "Dima", "Nurdos", "Madi"];
+
+//names.push("Beks"); //добавляет Бекс в консоль
+//names.push("Beks");
+names.shift();
+console.log(names);
+//names.unshift("Beks"); //Бекс идет в перед
+
+//names.shift("Beks"); //удаляет первый элемент
+
+//const name = names.pop();//удаляет последни индех
+
+//console.log("Names ", names, name);
+
+//const Letters = ["e", "d", "c", "b", "a"];
+//console.log(Letters.sort()); //меняет порядку как а,б,c,д,e
+//
+//console.log(names.reverse()); // меняет месты
+
+//console.log(names.splice(0, 1));  это удаляет  и е оставляет оригинальны режим
+//console.log(names.toSpliced(0, 2)); // это удаляет но оставляет оригинальный режим
+//console.log(names);
